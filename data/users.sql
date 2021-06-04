25eb03f4f40232464c255ea72570d200a9054df4
Сергеев Андрей Александрович <sergandr@WS113077.MO>
2021-06-04 11:40:23 +0300

    merge master

---
+++ data/users.sql
@@ -0,0 +1,18 @@
+INSERT INTO "main"."users_customuser"
+("id", "is_superuser", "email", "username", "role")
+VALUES (100, 0, 'bingobongo@yamdb.fake', 'bingobongo', 'user');
+INSERT INTO "main"."users_customuser"
+("id", "is_superuser", "email", "username", "role")
+VALUES (101, 1, 'capt_obvious@yamdb.fake', 'capt_obvious', 'admin');
+
+INSERT INTO "main"."users_customuser"
+("id", "is_superuser", "email", "username", "role")
+VALUES (102, 0, 'faust@yamdb.fake', 'faust', 'user');
+
+INSERT INTO "main"."users_customuser"
+("id", "is_superuser", "email", "username", "role")
+VALUES (103, 0, 'reviewer@yamdb.fake', 'reviewer', 'user');
+
+INSERT INTO "main"."users_customuser"
+("id", "is_superuser", "email", "username", "role")
+VALUES (104, 0, 'angry@yamdb.fake', 'angry', 'moderator');
